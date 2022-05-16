77ba4e49be07b2bcf525e9815f98a85d2dd998bd
ithelpTheDoctor <75475992+ithelpTheDoctor@users.noreply.github.com>
2022-05-16 16:00:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-"""
-TODO
-1. Change IP (DONE)
-2. Change geolocation (DONE)
-3. Change MAC Address (DONE)
-4. Browser fingerprint
-
-Refresh cookies (DONE) - though not any, it is still good practice to delete them
-Refresh session (DONE)
-
-"""
 
 import time, random, requests
-from sample.proxy1 import Proxy1
-from sample.proxy2 import Proxy2
-from sample.proxy3 import Proxy3
+from modules.proxy1 import Proxy1
+from modules.proxy2 import Proxy2
+from modules.proxy3 import Proxy3
 from selenium import webdriver
-from sample.geolocation import Geolocation
-from sample.mac_address import MacAddress
-from sample.user_agents import UserAgents
+from modules.geolocation import Geolocation
+from modules.user_agents import UserAgents
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -31,10 +19,6 @@ if __name__ == "__main__":
 
     #Starting URL
     url = 'https://google.com'
-
-    #MAC Address, User Agent
-    macaddress = MacAddress()
-    macaddress.mac_address('usb0')
     useragents = UserAgents()
     user_agent = useragents.random_user_agent()
     
@@ -42,14 +26,15 @@ if __name__ == "__main__":
     proxy1 = Proxy1()
     proxy2 = Proxy2()
     proxy3 = Proxy3()
+    
     proxy_choices = {'SSL Proxies': proxy1.proxy(), 'GitHub elite proxies':proxy2.proxy(), 'GimmeProxies': proxy3.proxy()}
-
+    
     #Confirm proxy's validity
     def check_proxy():
         proxy_found = False
         counter = 0
         PROXY, country = '',''
-        while proxy_found:
+        while not proxy_found: # loop wasn't running
             proxy_choice = random.choice(list(proxy_choices.values()))
             print(proxy_choice)
             PROXY, country = proxy_choice
@@ -59,16 +44,23 @@ if __name__ == "__main__":
                 r = requests.get('https://pinchofyum.com/', proxies={'https': 'https://{}'.format(PROXY)}, timeout=8)
                 if r.status_code == 200:
                     print('\n[+] IP Address used as proxy: ' + PROXY)
-                    proxy_not_found = True
+                    proxy_found = True  # variable named wrong
                 else:
-                    PROXY, country = proxy_choice()
+                    PROXY, country = proxy_choice # lists can't be called like an object
             except:
-                PROXY, country = proxy_choice()
+                PROXY, country = proxy_choice  # lists can't be called like an object
+            
+            if counter==3:
+                break
         return PROXY, country
 
     PROXY, country = check_proxy()
-    print('[+] Proxy of choice: ' + str(PROXY))
-
+    if not PROXY:
+        print("\nNo working proxy found") # need to increase proxy collection numbers.
+        quit()
+        
+    print('\n[+] Proxy of choice: ' + str(PROXY))
+    
     #Set Chromedriver Options
     options = Options()
     # options.add_argument('--headless')
@@ -99,7 +91,7 @@ if __name__ == "__main__":
     }
 
     #Fire up chromedriver
-    chromedriver = webdriver.Chrome("./chromedriver", options=options, desired_capabilities=capabilities)
+    chromedriver = webdriver.Chrome("chromedriver.exe", options=options, desired_capabilities=capabilities)
 
     #Confirm user agent
     agent = useragents.get_user_agent(chromedriver)
@@ -111,10 +103,5 @@ if __name__ == "__main__":
     open_url(chromedriver, url)
     time.sleep(5)
     #### Add your own code here.
-
-    # Get session
-    print('[+] Session ID: ' + chromedriver.session_id)
-    print('[+] Deleting all cookies...')
     chromedriver.delete_all_cookies()
     chromedriver.quit()
-
